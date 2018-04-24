ea33c515e3ddab60007ddf5a3aee81e205384a44
Nick Pope <pope410211@gmail.com>
2018-04-23 22:13:59 -0400

    building the login forms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,33 +12,46 @@
 			'ui.router',
 			'firebase',
 			'ngMaterial',
-			'ngMessages'
+			'ngMessages',
+			'FireAuth'
 		])
 		.config(function ($stateProvider, $urlRouterProvider) {
+			var headerObj = {
+				templateUrl: 'views/includes/header.html',
+				controller: 'HeaderCtrl',
+				controllerAs: 'header'
+			};
+			var footerObj = {
+				templateUrl: 'views/includes/footer.html',
+				controller: 'FooterCtrl',
+				controllerAs: 'footer'
+			};
 			$urlRouterProvider.when('/', '/home');
 			$urlRouterProvider.otherwise('/home');
-			console.log('Sanity Check');
 			$stateProvider
 			.state('home', {
 				url: '/home',
 				views: {
-					'header': {
-						templateUrl: 'views/includes/header.html',
-						controller: 'HeaderCtrl',
-						controllerAs: 'header'
-					},
+					'header': headerObj,
 					'main': {
 						templateUrl: 'views/main.html',
 						controller: 'MainCtrl',
 						controllerAs: 'main'
 					},
-					'footer': {
-						templateUrl: 'views/includes/footer.html',
-						controller: 'FooterCtrl',
-						controllerAs: 'footer'
-					}
+					'footer': footerObj
+				}
+			})
+			.state('login', {
+				url:'/vendor-login',
+				views: {
+					'header': headerObj,
+					'main': {
+						templateUrl: 'views/admin/login.html',
+						controller: 'LoginCtrl',
+						controllerAs: 'login'
+					},
+					'footer': footerObj
 				}
 			});
-			console.log('state', $stateProvider);
 		});
 }());
---
+++ app/scripts/services/authService.js
@@ -0,0 +1,8 @@
+(function() {
+'use strict';
+	angular
+		.module('FireAuth', [])
+		.service('FirebaseAuthService', function() {
+
+		});
+}());
