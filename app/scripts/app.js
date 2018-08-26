21015fc5fe87f492270a817f023fb33f666e2b0f
Nick Pope <pope410211@gmail.com>
2018-08-26 15:15:31 -0400

    Unsure. Returning after months. Doing a quick commit before starting so I can review later if needed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
 (function() {
 'use strict';
-
+	var config = {
+		apiKey: "AIzaSyCEe3plaRve9G8vZLiP5PMQy4v2K_MwYPI",
+		authDomain: "turtlegirlsmarket-64f1c.firebaseapp.com",
+		databaseURL: "https://turtlegirlsmarket-64f1c.firebaseio.com",
+		projectId: "turtlegirlsmarket-64f1c",
+		storageBucket: "turtlegirlsmarket-64f1c.appspot.com",
+		messagingSenderId: "134773211753"
+	};
+		firebase.initializeApp(config);
 	angular
 		.module('ngTgmApp', [
 			'ngAnimate',
